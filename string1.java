acea7991d7012921d20241792bde9a141e726694
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-10-17 22:07:50 +0530

    basic string concept

---
+++ string1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class string1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = sc.nextLine();
+        // System.out.println(str);
+        // //  for length  of the string
+        String s = "harshop";
+        String h ="harshop";
+        // int har = s.length();
+      
+    // System.out.println(har);
+    System.out.println(s.charAt(4));
+    System.out.println(s.indexOf(('a')));
+    System.out.println(s.compareTo(h));
+
+        
+    }
+}
