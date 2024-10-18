c80cfbcf5e2b586199e03d93ace6073808ab79a9
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-10-18 18:40:43 +0530

    more string concept

---
+++ strings2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class strings2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String s = sc.nextLine();
+        String str =  "Harsh bhardwaj";
+        String str2 = " and ms dhoni  ";
+        System.out.println(str.contains("xyz"));
+        System.out.println(str.startsWith("bha"));
+        System.out.println(str.endsWith("waj"));
+        System.out.println(str.toLowerCase());git
+        System.out.println(str.toUpperCase());
+        System.out.println(str.concat(str2));
+
+
+        
+    }
+}
