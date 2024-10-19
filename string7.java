01ee48fd9a871509fb69b13d3ddc0105d09efda1
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-10-19 21:53:09 +0530

    string equals concept

---
+++ string7.java
@@ -0,0 +1,12 @@
+public class string7{
+    public static void main(String[] args) {
+        String s1 = "hii";
+        String s2 = "hii";
+        String  s3= new String("hii");
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s3));
+
+        
+    }
+}
